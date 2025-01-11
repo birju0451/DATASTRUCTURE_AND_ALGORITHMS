256563c69c25d246d95c523147f9cb3cfddcaf0e
birju0451 <165183340+birju0451@users.noreply.github.com>
2025-01-11 23:06:51 +0530

    initial commit

--- src/ARRAY/MaxInarray.java
+++ src/ARRAY/MaxInarray.java
@@ -2,13 +2,13 @@ package ARRAY;
 
 public class MaxInarray {
     public static void main(String[] args) {
-        int[] arr={-41,-45,-85,-94,-75,-10,};
+        int[] arr={-41,-45,-85,-94,-75,-10,78};
         int max=arr[0];
         for (int j : arr) {
             if (j > max) {
                 max = j;
             }
         }
-        System.out.println(max);
+        System.out.println("the max element in the array is :"+max);
     }
 }
